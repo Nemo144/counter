df4f7c1a67d3d63573dcf9ac0a6a553957eae05b
Nnamdi Okeke <onnamdialex@gmail.com>
2025-01-22 18:53:15 +0100

    :white_check_mark: Add test for contract and update npm

--- tests/counter.test.ts
+++ tests/counter.test.ts
@@ -1,4 +1,3 @@
-
 import { describe, expect, it } from "vitest";
 
 const accounts = simnet.getAccounts();
@@ -18,4 +17,26 @@ describe("example tests", () => {
   //   const { result } = simnet.callReadOnlyFn("counter", "get-counter", [], address1);
   //   expect(result).toBeUint(0);
   // });
+});
+
+Clarinet.test({
+  name: "get-count returns u0 for principals that never called count-up before",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    // Get the deployer account.
+    let deployer = accounts.get("deployer")!;
+
+    // Call the get-count read-only function.
+    // The first parameter is the contract name, the second the
+    // function name, and the third the function arguments as
+    // an array. The final parameter is the tx-sender.
+    let count = chain.callReadOnlyFn(
+      "counter",
+      "get-count",
+      [types.principal(deployer.address)],
+      deployer.address
+    );
+
+    // Assert that the returned result is a uint with a value of 0 (u0).
+    count.result.expectUint(0);
+  },
 });
